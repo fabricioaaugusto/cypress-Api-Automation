441d92a20aa91eef0f7ebc7ef0dc76bff0cbfea2
Fabricio <fabricioaaugusto@gmail.com>
2021-10-09 23:52:55 -0300

    init project

---
+++ cypress/integration/apitests/getusers.js
@@ -0,0 +1,33 @@
+/// <reference types ="Cypress" />
+
+describe('Get API User Tests', () => {
+
+    it('get users by tests Go Rest.com', () => {
+        cy.request({
+
+            method : 'GET',
+            url : 'https://gorest.co.in/public/v1/users',
+            headers : {
+                'authorization' : "Bearer 09adec0bcf1f43602bb4fa73cadae1bde320ba6c51a283ee105f10773ea52f1b"
+            }
+        }).then((res)=>{
+            expect(res.status).to.eq(200)
+            expect(res.body.meta.pagination.limit).to.eq(20)
+        })
+    })
+
+    it('get users assert name user', () => {
+        cy.request({
+
+            method : 'GET',
+            url : 'https://gorest.co.in/public/v1/users/60',
+            headers : {
+                'authorization' : "Bearer 09adec0bcf1f43602bb4fa73cadae1bde320ba6c51a283ee105f10773ea52f1b"
+            }
+        }).then((res)=>{
+            expect(res.status).to.eq(200)
+            expect(res.body.data.name).to.eq('Gemini Gill')
+        })
+    })
+
+})
